2e835a4cc1a5eb48f0ae99779d529caa8d21c6ca
atilio70 <atiliostefani@gmail.com>
2024-10-07 09:37:22 -0300

    Subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,9 @@
+
+
+Nombre_dado = input("Cual es tu nombre? ")
+Nombre = "Ailu"
+if Nombre==Nombre_dado:
+    print("Hola Mailob, te quiero mucho.")
+else:
+    saludo= 'Buen día ' + Nombre_dado + " ¿como se encuentra usted?"
+    print(saludo)
